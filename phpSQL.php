868fba46e948811f079a194663be2f4a5a78e05d
jeisonmarques <jeison.marques@gmail.com>
2015-09-30 10:39:10 -0300

    Fix: Teste para Select

--- phpSQL.php
+++ phpSQL.php
@@ -4,6 +4,18 @@ try
 	print( "Connecting to SQL Server." );
 	$conn = new PDO ( "sqlsrv:server = tcp:v8xjjhajlw.database.windows.net,1433; Database = motelAtVQaSrngqJ", "apl", "Sjm157304@");
 	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	print( "Connected to SQL Server." );
+	
+	$query = $conn->prepare("select * FROM Motel");
+    $query->execute();
+ 
+      for($i=0; $row = $query->fetch(); $i++){
+        echo $i." - ".$row['Nome']."<br/>";
+      }
+ 
+      unset($conn); 
+      unset($query);
+	
 }
 catch ( PDOException $e )
 {
