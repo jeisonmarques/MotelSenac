e88c562d5a908f9464bfa7366ce862126039e29c
jeisonmarques <jeison.marques@gmail.com>
2015-09-30 14:57:36 -0300

    Fix: Connnection

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ function printr($p)
 use Connection\Dao\User;
 
 $user = new User();
-$result = $user->query("SELECT * FROM cs_users");
+$result = $user->query("SELECT * FROM Motel");
 
 foreach ($result as $key => $val) {
     printr(sprintf("Chave: %s => Valor: %s", $key, $val));
--- src/Connection/Database/PdoConnection.php
+++ src/Connection/Database/PdoConnection.php
@@ -16,7 +16,11 @@ class PdoConnection
     private function connect()
     {
         try {
-            $this->pdo = new PDO("sqlsrv:server=tcp:v8xjjhajlw.database.windows.net,1433;Database=motelAtVQaSrngqJ", "apl", "Sjm157304@");
+			$server = "tcp:v8xjjhajlw.database.windows.net,1433";
+			$username = "apl@v8xjjhajlw";
+			$password = "Sjm157304@";
+			$database = "motelAtVQaSrngqJ";
+            $this->pdo = new PDO("sqlsrv:server=$server ; Database = $database", $username, $password);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(\PDOException $e) {
             throw new \PDOException($e->getMessage());
