cc498233a267bfc6ea7e7da0df7d61ee9f78ed4a
jeisonmarques <jeisonmarques@MacBook-Pro-de-Jeison.local>
2015-10-01 20:47:22 -0300

    ...

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ function printr($p)
 use Connection\Dao\User;
 
 $user = new User();
-$result = $user->queryAll("SELECT * FROM Motel");
+$result = $user->query("SELECT * FROM Motel");
 
 foreach ($result as $key => $val) {
     printr(sprintf("Chave: %s => Valor: %s", $key, $val));
