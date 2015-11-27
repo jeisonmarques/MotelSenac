7ea087a1708ac4d6cdf8c3b99786e9f7e4209d7b
Marcelo Colombo <marcelo.pcolombo@gmail.com>
2015-11-26 23:06:04 -0200

    'erituiertyui'

--- admin/pages/dao/ClienteQuartosDAO.php
+++ admin/pages/dao/ClienteQuartosDAO.php
@@ -36,7 +36,7 @@ class ClienteQuartosDAO extends BancoPDO {
 
             if($stm->execute()) {
                 $tabela =
-                "<table class="table table-striped table-bordered table-hover id="dataTables-example><tr>"
+                "<table class='table table-striped table-bordered table-hover' id='dataTables-example'><tr>"
                          ."<td>ID</td>"
                          ."<td>CLIENTE_ID</td>"
                          ."<td>DESCRICAO</td>"
